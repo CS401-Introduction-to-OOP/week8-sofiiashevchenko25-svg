f7d9ae3cb1617e85b314aa43838fda9c8ff220ed
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 14:02:55 +0200

    commit

---
+++ Week8/DeliveryItem.cs
@@ -0,0 +1,18 @@
+﻿public abstract class SmartDevice
+{
+    public string Name { get ; }
+    public bool IsOn { get ; protected set ; } // State can only bemodified internally or in derived classes
+    public SmartDevice ( string name )
+    {
+        Name = name ;
+        IsOn = false ;
+    }
+// Abstract method : each device toggles in its own way
+    public abstract void Toggle () ;
+// Virtual method : base logic that can be extended
+    public virtual void PrintStatus ()
+    {
+        string status = IsOn ? " On " : " Off " ;
+        Console.WriteLine( $" Device : { Name } | Status : { status } ");
+    }
+}
