814e8c0db9c6005d8e44d22240e0b3559f0dd34b
sofis <sofiia.shevchenko.25@kse.org.ua>
2026-03-28 14:29:47 +0200

    last part of part 1

---
+++ Week8/Parcel.cs
@@ -0,0 +1,30 @@
+﻿namespace Week8;
+
+
+public class Percel : DeliveryItem
+{
+    public string dimensions { get ; private set ; }
+    
+// Calling the base class constructor via base ()
+    public Percel(string TrackingNumber, double Weight) : base( TrackingNumber, Weight)
+    {
+
+        string imensions = dimensions ;
+    }
+    
+// Mandatory implementation of the abstract method
+    public override double CalculateCost ()
+    {
+
+        double endcost = 50 + Weight * 25;
+        return endcost;
+    }
+    
+// Overriding the virtual method with a call to base logic
+    public override void PrintInfo()
+    {
+        base.PrintInfo();
+        Console.WriteLine($" Brightness : { dimensions }% " );
+    }
+    
+}
